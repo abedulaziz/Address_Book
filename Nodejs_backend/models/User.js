a7775abdfd1c60d2de260d185c2ff4e1de1cfb02
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-06-26 02:37:03 +0300

    Establishing registration and jsonwebtoken

--- Nodejs_backend/models/User.js
+++ Nodejs_backend/models/User.js
@@ -5,12 +5,43 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    min: 6,
+    max: 255,
   },
-  age: {
-    type: Number,
-    default: 0
-  }
+  email: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 1024,
+  },
+  phone_number: {
+    type: String,
+    required: true,
+    min: 8,
+    max: 12
+  },
+  contacts: [{
+    full_name: String,
+    phone_number: String,
+    relationship_status: {
+      type: String,
+      enum: ["single", "married", "engaged"]
+    },
+    email: String,
+    location:{
+      type: Array,
+      coordinates: [32, 20]
+    } 
+  }]
+  
 })
 
 const User = mongoose.model("User", UserSchema)
--- Nodejs_backend/routes.js
+++ Nodejs_backend/routes.js
@@ -1,6 +1,15 @@
+require("dotenv").config()
+
 const express = require("express");
-const userModel = require("./models/User");
 const app = express();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+
+const User = require("./models/User");
+
+
+app.use(express.json())
+
 
 app.post("/api/users/add_user", async (req, res) => {
   console.log(req);
@@ -16,14 +25,56 @@ app.post("/api/users/add_user", async (req, res) => {
 });
 
 
-app.get("/api/users", async (request, response) => {
+app.get("/api/users", async (req, res) => {
   const users = await userModel.find({});
 
   try {
-    response.send(users);
+    res.send(users);
   } catch (error) {
-    response.status(500).send(error);
+    res.status(500).send(error);
   }
 });
 
 module.exports = app;
+
+
+// register api
+app.post("/api/users/register", async (req, res) => {
+
+  const hashedPwd = await bcrypt.hash(req.body.password, 10)
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPwd,
+    phone_number: req.body.phone_number,
+  })
+
+  try {
+    user.save()
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+app.post("/api/users/login", async (req, res) => {
+  
+  // const findEmail =  await User.find({email: req.body.email})
+
+  const user = {email: req.body.email}
+  const access_token = jwt.sign(user ,process.env.ACCESS_TOKEN_SECRET)
+
+
+  try {
+    res.json({"access_token": access_token})
+  }catch (err) {
+    console.log(err);
+    res.send(err)
+  }
+
+})
+
+
+module.exports = app
