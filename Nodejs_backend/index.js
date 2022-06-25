f52a7e386e242aee6b61ae7e0d971288511e8ccf
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-06-25 15:16:00 +0300

    Initialising project and install important modules in the backend

---
+++ Nodejs_backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const Router = require("./routes")
+
+const app = express();
+
+app.use(express.json());
+
+const username = "AbdulAziz";
+const password = "rjbjcBmR4F5aRyw";
+const cluster = "cluster0.magzs";
+const dbname = "myFirstDatabase";
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.get("/", (req, res) => {
+  res.send("haha")
+
+
+})
+
+app.listen(5000)
