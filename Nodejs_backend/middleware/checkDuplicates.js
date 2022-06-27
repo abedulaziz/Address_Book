e5671e6c007f63f137195fd6fb8e9acc96107c4f
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-06-27 15:25:40 +0300

    Enhancing checkDuplicates middleware

--- Nodejs_backend/middleware/checkDuplicates.js
+++ Nodejs_backend/middleware/checkDuplicates.js
@@ -1,17 +1,14 @@
 const User = require("../models/User");
 
 
-
 const checkDuplicate = async(req, res, next) => {
 
-  const isDuplicate = await User.find({email: req.body.email})
+  const isDuplicate = await User.exists({email: req.body.email})
   
-  if (isDuplicate.length > 0) {
-    res.status(400).send("This email is already taken")
+  if (isDuplicate) {
+    return res.status(400).send("This email is already taken")
   }
-
   next()
-
 }
 
 module.exports = checkDuplicate
