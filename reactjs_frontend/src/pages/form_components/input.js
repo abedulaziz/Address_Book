dfd0e86ecfba586e8ccfd3bba4602cef6c03b63e
abedulaziz <abdulaziz.kharraz@outlook.com>
2022-06-27 09:59:30 +0300

    Initial submission fetch in sign-up page

--- reactjs_frontend/src/pages/form_components/input.js
+++ reactjs_frontend/src/pages/form_components/input.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Input = ({className, type, name, placeholder}) => {
+const Input = React.forwardRef(({className, type, name, placeholder}, ref)=> {
   return (
     <div className={className}>
-      <input type={type} name={name} placeholder={placeholder} />
+      <input ref={ref} type={type} name={name} placeholder={placeholder} />
     </div>
   )
-}
+}) 
 
 export default Input    
--- reactjs_frontend/src/pages/sign_up.js
+++ reactjs_frontend/src/pages/sign_up.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import Head from "react-helmet";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import Input from './form_components/input';
 
 const SignUp = () => {
+
+  const fname =useRef(null);
+  const lname =useRef(null);
+  const email = React.createRef()
+  const password = React.createRef()
+  const password_confirm = React.createRef()
+  const phone_num = React.createRef()
+  const gender =useRef(null);
+  const dob = React.createRef()
+
+  const submitForm = async(e) => {
+    e.preventDefault();
+    try {
+      const registerReq = await axios({
+        method: "POST",
+        url: "http://localhost:5000/api/users/register",
+        body: {
+          name: fname.current.value,
+          email: email.current.value,
+          password: password.current.value,
+          phone_number: phone_num
+        }
+      })
+
+      console.log(registerReq)
+    }
+    catch(err){
+      console.error(err);
+
+    } 
+  }
 
   return (
     <>
@@ -26,26 +58,27 @@ const SignUp = () => {
               <h1>Sign Up</h1>
               <p className='subtitle'>Sign-up and create your contacts</p>
               <div className="username">
-                <input type="text" name="fname" placeholder="First name" max="30" />
-                <input type="text" name='lname' placeholder="Last name" max="30" />
+                <input ref={fname} type="text" name="fname" placeholder="First name" max="30" />
+                <input ref={lname} type="text" name='lname' placeholder="Last name" max="30" />
               </div>
-              <Input className="email" type="text" name="email" placeholder="Email" />
+              <Input ref={email} className="email" type="text" name="email" placeholder="Email" />
 
-              <Input className="password" type="password" name="password" placeholder="Password" />
+              <Input ref={password} className="password" type="password" name="password" placeholder="Password" />
 
-              <Input className="password_confirmation" type="password" name="password_confirmation" placeholder="password_confirmation" />
+              <Input ref={password_confirm} className="password_confirmation" type="password" name="password_confirmation" placeholder="password_confirmation" />
+              <Input ref={phone_num} className="phone_number" type="text" name="phone_number" placeholder="Phone Number" />
 
               <div className="gender">
-                <select name="gender" id="gender">
+                <select name="gender" id="gender" ref={gender}>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                 </select>
               </div>
 
-              <Input className="dob" type="date" name="date_of_birth" />
+              <Input ref={dob} className="dob" type="date" name="date_of_birth" />
 
               <div className="submit">
-                <input id="register" type="button" value="Register" />
+                <input id="register" type="button" value="Register" onClick={submitForm}/>
               </div>
               <p className='redirect'>Already have an existing account? <Link to="/">sign-in</Link></p>
 
